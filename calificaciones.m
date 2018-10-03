1906c68966dcfd85aed3d034551780b8b9b77fca
LucioPurecoGuadalupe <43255297+LucioPurecoGuadalupe@users.noreply.github.com>
2018-10-03 11:19:21 -0500

    Delete calificaciones.m

--- calificaciones.m
+++
@@ -1,7 +0,0 @@
-clear;clc;
-x=input('introduzca una calificacion:  ');
-if rem(x,70)==0
-    disp('felicidades has aprobado')
-else
-    disp('continua con tus estudios,necesitas esforzarte mas')
-end
